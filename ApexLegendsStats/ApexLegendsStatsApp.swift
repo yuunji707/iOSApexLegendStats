3d8342c009d0d01ee300d84c8fc18f8dc6b1bee9
younis <yereiqat@gmail.com>
2024-07-23 02:10:42 -0700

    added launch screen

--- ApexLegendsStats/ApexLegendsStatsApp.swift
+++ ApexLegendsStats/ApexLegendsStatsApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct ApexLegendsStatsApp: App {
+    @State private var isActive = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isActive {
+                ContentView()
+            } else {
+                LaunchScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            withAnimation {
+                                self.isActive = true
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct LaunchScreenView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Image("LaunchScreenImage")
+                .resizable()
+                .scaledToFill()
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .clipped()
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
